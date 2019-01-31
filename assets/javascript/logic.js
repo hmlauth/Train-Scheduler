92ede91f4fde639f1a0068f246f1233f4db8825f
Andromeda <hannalauth@gmail.com>
2019-01-30 19:40:14 -0800

    moment.js applied to calculate next arrival and minutes away

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,4 +1,6 @@
 // alert("I am working");
+// use diff moment.js
+// add, subtract, year
 
 // Initialize Firebase
 var config = {
@@ -28,27 +30,64 @@ var minutesAway = "";
 // ==============================================================
 // Clear input values
 function clearInput() {
-    $("#train-name-input").val(" ");
-    $("#destination-input").val(" ");
-    $("#first-train-input").val(" ");
-    $("#frequency-input").val(" ");
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#first-train-input").val("");
+    $("#frequency-input").val("");
 }
 
 // METHODS
 // ==============================================================
 // At load of page and subsequent value changes, display snapshot of value stored in firebase.
-database.ref().on("value", function(snapshot) {
-        trainName = snapshot.val().trainName;
-        console.log(trainName);
+// Ref takes in particular path to update inside of our database. A place to store data in our database. 
+// "child_added" is an event and the "function(snapshot)" is a call back function b/c it is listed as the second parameter.
+// child_added will give you all the data added at once
+// set will overwrite the reference that you're dealing with, versus push will add to the reference that you're dealing with.
+// push and child_added are connected - make sure you're ref the same location in database
+database.ref().on("child_added", function (snapshot) {
+    // snapshot.val() grabs "thing" that was saved to database; just snapshot will provide metadata about the "thing" we care about.
+    // snapshot gives things in the order that they were added to the database unless otherwise specified. 
+    trainName = snapshot.val().trainName;
+    destination = snapshot.val().destination;
+    firstTrainTime = snapshot.val().firstTrainTime;
+    frequency = snapshot.val().frequency;
 
-        trainRow = $("<tr>");
-        trainData = $("<td>");
-        destinationData = $("<td>");
-        trainData.text(trainName);
-        destinationData.text(destination);
-        trainRow.append(trainData);
-        trainRow.append(destinationData);
-        $("tbody").append(trainRow);
+        // take firstTrainTime in format HH:mm and subtract 1 year to ensure the time comes before the current time.  
+        var firstTimeConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
+        console.log(firstTimeConverted);
+        // find difference between current time and firstTimeConverted
+        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        console.log(diffTime);
+        // Once we have the difference we need to see how many trains have come and gone and what is left over to then use that number to predict the next arrival. For example, if a train comes every 5 minutes starting at 10:00 and it's 10:11, then we know two trains have come with 1 minute remainder. Knowing the remainer will then give us a point of reference to know in how many minutes the next train will come through simple subtraction of the frequency minus the remainder.
+        var tRemainder = diffTime % frequency;
+        console.log(tRemainder);
+        var minutesTillTrain = frequency - tRemainder;
+        console.log(minutesTillTrain);
+        var nextTrain = moment(moment().add(minutesTillTrain, "minutes")).format("hh:mm");
+        console.log(nextTrain);
+
+    // Create new table elements
+    tRow = $("<tr>");
+    tTrain = $("<td>");
+    tDestination = $("<td>");
+    tFrequency = $("<td>");
+    tNextArrival = $("<td>");
+    tMinutesTillTrain = $("<td>");
+
+    // Put text in each element
+    tTrain.text(trainName);
+    tDestination.text(destination);
+    tFrequency.text("Every " + frequency + " min.");
+    tNextArrival.text(nextTrain);
+    tMinutesTillTrain.text(minutesTillTrain + " min. away!");
+    // Append child_added snapshot data to the new row
+    tRow.append(tTrain);
+    tRow.append(tDestination);
+    tRow.append(tFrequency);
+    tRow.append(tNextArrival);
+    tRow.append(tMinutesTillTrain);
+    // Append new row to the tbody
+    $("tbody").append(tRow);
 
 });
 
@@ -65,13 +104,19 @@ $("#add-train").on("click", function (event) {
     console.log(firstTrainTime);
     console.log(frequency);
 
-    database.ref().set({
+    // Instead of set(), use push() which adds data to the root
+    database.ref().push({
         trainName: trainName,
         destination: destination,
         firstTrainTime: firstTrainTime,
-        frequency: frequency,
-        nextArrival: nextArrival,
-        minutesAway: minutesAway
+        frequency: frequency
+        // Next Arrival and Minutes Away will be calculated after we pull the snapshot of the added information and then display in on the page. 
+    }, function (error) {
+        if (error) {
+            // The write failed...
+        } else {
+            // Data saved successfully!
+        }
     });
 
     clearInput();
